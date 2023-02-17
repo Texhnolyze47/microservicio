8a60eb800a1c2fcfc41679f8e62a45414405ff0a
Texhnolyze47 <ichigobankai136@gmail.com>
2023-02-16 22:27:24 -0600

    Estructura inicial del proyecto

---
+++ src/test/java/com/texhnolyze/microserviciodemo/MicroservicioDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.texhnolyze.microserviciodemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicioDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
